d7fd096c7b2d39a8dfbf6b4dd56e7e0c9c601fe6
Sourove <souroveskb@gmail.com>
2024-02-18 18:01:53 +0600

    Enums and Pattern matching
    So first I learned to use enum, add #[derive(Debug)] to allow printing that enum,
    impl block for an enum, attaching a data with the enum, match case, Option<t> enum
    to avoid the NULL value problem.
    
    option<t> wraps the null value so that we always need to check before using the value.
    
    Then the last part was to learn if let statement to use single match i.e. without the
    exhaustive search.

--- chap-6/src/main.rs
+++ chap-6/src/main.rs
@@ -1,6 +1,7 @@
 #![allow(unused)]
 
-use std::io;
+use core::num;
+use std::{io, net};
 #[derive(Debug)]
 enum IpAddrKind{
     V4(String), 
@@ -55,8 +56,45 @@ fn main() {
     let some_char = Some("c");
 
     let absent_num: Option<i32> = None;
+
+    println!("matching func called {}", match_ip(IpAddrKind::V4(String::from("102"))));
+
+    let x = plus_one(absent_num);
+    println!("plus one called on none: {}", match x {
+        Some(x) => x,
+        other => 0,
+    });
+
+    let x: Option<i32> = plus_two(some_num);
+    println!("plus two called on somenum: {}", match x {
+        Some(x) => x,
+        other => 0,
+    });
 }
 
 fn route(ip_kind: IpAddrKind){
     println!("{:?}", ip_kind);
+}
+
+fn match_ip(ip_kind: IpAddrKind) -> u8 {
+    match ip_kind {
+        IpAddrKind::V4(strs) => 4, 
+        IpAddrKind::V6(strs) => 6,
+    }
+}
+
+fn plus_one(num: Option<i32>) -> Option<i32> {
+    match num {
+        None => None,
+        Some(num) => Some(num+1),
+    }
+}
+
+fn plus_two(num: Option<i32>) -> Option<i32> {
+    if let Some(n) = num {
+        println!("Added two {}", n);
+        Some(n+2)
+    } else{
+        None
+    }
 }
